5084d08e846529d0b7f07be69e2dc6f7168277e0
Bernd K <prof7bit@gmail.com>
2017-02-01 21:14:47 +0100

    implement more stuff, disable timer for now, probably need something else.

--- formmain.pas
+++ formmain.pas
@@ -28,6 +28,8 @@ type
 
   TFMain = class(TForm)
     ImageList: TImageList;
+    MenuItemConsole: TMenuItem;
+    MenuItemMeld: TMenuItem;
     MenuItemPull: TMenuItem;
     MenuItemCommit: TMenuItem;
     MenuItemPush: TMenuItem;
@@ -37,22 +39,24 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure MenuItemConsoleClick(Sender: TObject);
+    procedure MenuItemMeldClick(Sender: TObject);
+    procedure MenuItemPullClick(Sender: TObject);
+    procedure MenuItemPushClick(Sender: TObject);
+    procedure NodeMenuPopup(Sender: TObject);
     procedure TreeViewExpanded(Sender: TObject; Node: TTreeNode);
     procedure TreeViewGetImageIndex(Sender: TObject; Node: TTreeNode);
     procedure TreeViewGetSelectedIndex(Sender: TObject; Node: TTreeNode);
-    procedure TreeViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
     procedure UpdateTimerTimer(Sender: TObject);
   private
     FQueueLock: TCriticalSection;
     FUpdateQueue: TNodeQueue;
     FUpdateThread: TUpdateThread;
-    procedure QueryStatus(N: TShellTreeNode; QueueUpdate: Boolean);
+    procedure QueueForUpdate(Node: TShellTreeNode);
+    procedure QueryStatus(N: TShellTreeNode; RemoteUpdate: Boolean);
     procedure AsyncQueryStatus(P: PtrInt);
     procedure UpdateAllNodes(Root: TTreeNode);
     function GitExe: String;
-    { private declarations }
-  public
-    { public declarations }
   end;
 
 var
@@ -74,33 +78,62 @@ begin
   {$endif}
 end;
 
+procedure StartExe(Path: String; Cmd: String; args: array of string; Wait: Boolean);
+var
+  P: TProcess;
+  I: Integer;
+  A: String;
+begin
+  P := TProcess.Create(nil);
+  P.CurrentDirectory := Path;
+  P.Options := [poUsePipes, poNoConsole, poNewProcessGroup, poWaitOnExit];
+  P.Executable := cmd;
+  for I := 0 to GetEnvironmentVariableCount -1 do begin
+    if Pos('LANG=', GetEnvironmentString(I)) = 0 then
+      P.Environment.Append(GetEnvironmentString(I));
+  end;
+  P.Environment.Append('LANG=C');
+  for A in args do begin
+    P.Parameters.Add(A);
+  end;
+  P.Execute;
+  P.Free;
+end;
+
 function RunTool(Path: String; cmd: String; args: array of string; out ConsoleOutput: String): Boolean;
 var
   P: TProcess;
   A: String;
-  EndTime: TDateTime;
+  I: Integer;
 begin
-  EndTime := Now + 5000 * MILLISEC;
   P := TProcess.Create(nil);
   P.CurrentDirectory := Path;
   P.Options := [poUsePipes, poNoConsole];
   P.Executable := cmd;
-  P.Environment.Add('LANG=C');
-  for A in args do
+  for I := 0 to GetEnvironmentVariableCount -1 do begin
+    if Pos('LANG=', GetEnvironmentString(I)) = 0 then
+      P.Environment.Append(GetEnvironmentString(I));
+  end;
+  P.Environment.Append('LANG=C');
+  for A in args do begin
     P.Parameters.Add(A);
+  end;
   P.Execute;
   ConsoleOutput := '';
   repeat
     Sleep(1);
-    while P.Output.NumBytesAvailable > 0 do
-      ConsoleOutput += Chr(P.Output.ReadByte);
     if ThreadID = MainThreadID then
       Application.ProcessMessages;
-    if (Now > EndTime) or AppTerminating then
+    if AppTerminating then
       P.Terminate(1);
-  until not (P.Running or (P.Output.NumBytesAvailable > 0));
+  until not P.Running;
+  while P.Output.NumBytesAvailable > 0 do
+    ConsoleOutput += Chr(P.Output.ReadByte);
+  while P.Stderr.NumBytesAvailable > 0 do
+    ConsoleOutput += Chr(P.Stderr.ReadByte);
   Result := (P.ExitCode = 0);
   P.Free;
+  Print(ConsoleOutput);
 end;
 
 {$R *.lfm}
@@ -141,43 +174,48 @@ begin
   end;
 end;
 
-procedure TFMain.QueryStatus(N: TShellTreeNode; QueueUpdate: Boolean);
+procedure TFMain.QueueForUpdate(Node: TShellTreeNode);
+begin
+  FQueueLock.Acquire;
+  FUpdateQueue.Push(Node);
+  FQueueLock.Release;
+end;
+
+procedure TFMain.QueryStatus(N: TShellTreeNode; RemoteUpdate: Boolean);
 var
   Path: String;
   O: String;
   Behind: Boolean = False;
   Ahead: Boolean = False;
-  Clean: Boolean = False;
+  Dirty: Boolean = False;
+  Conflict: Boolean = False;
+  Diverged: Boolean = False;
 begin
   Path := N.FullFilename;
   if DirectoryExists(Path + DirectorySeparator + '.git') then begin
     if RunTool(Path, GitExe, ['status'], O) then begin
       N.Text := N.ShortFilename;
-      if Pos('behind', O) > 0 then
-        Behind := True;
-      if Pos('ahead', O) > 0 then
-        Ahead := True;
-      if Pos('clean', O) > 0 then
-        Clean := True;
-      if Behind then
-        N.Text := '[BEHIND] ' + N.Text;
-      if Ahead then
-        N.Text := '[AHEAD] ' + N.Text;
-      if not Clean then
-        N.Text := '[DIRTY] ' + N.Text;
-    end;
-    if Clean and not (Behind or Ahead) then begin
-      N.Data := Pointer(ICON_NORMAL + 1);
-    end
-    else begin
-      N.Data := Pointer(ICON_CHANGED + 1);
+      if Pos('Unmerged', O) > 0 then  Conflict := True;
+      if Pos('behind', O) > 0 then    Behind := True;
+      if Pos('ahead', O) > 0 then     Ahead := True;
+      if Pos('diverged', O) > 0 then  Diverged := True;
+      if Pos('Changes', O) > 0 then   Dirty := True;
+      if Behind then    N.Text := '[BEHIND] ' + N.Text;
+      if Ahead then     N.Text := '[AHEAD] ' + N.Text;
+      if Diverged then  N.Text := '[DIVERGED] ' + N.Text;
+      if Dirty then     N.Text := '[DIRTY] ' + N.Text;
+      if Conflict then  N.Text := '[CONFLICT] ' + N.Text;
     end;
 
-    if QueueUpdate then begin
-      FQueueLock.Acquire;
-      FUpdateQueue.Push(N);
-      FQueueLock.Release;
-    end;
+    if Conflict then
+      N.Data := Pointer(ICON_CONFLICT + 1)
+    else if Dirty or Behind or Ahead or Diverged then
+      N.Data := Pointer(ICON_CHANGED + 1)
+    else
+      N.Data := Pointer(ICON_NORMAL + 1);
+
+    if RemoteUpdate then
+      QueueForUpdate(N);
   end;
 end;
 
@@ -209,6 +247,58 @@ end;
 procedure TFMain.FormShow(Sender: TObject);
 begin
   UpdateAllNodes(TreeView.TopItem);
+end;
+
+procedure TFMain.MenuItemConsoleClick(Sender: TObject);
+var
+  Exe: String;
+begin
+  {$ifdef linux}
+  Exe := 'x-terminal-emulator';
+  {$endif}
+  StartExe(TreeView.Path, Exe, [], False);
+end;
+
+procedure TFMain.MenuItemMeldClick(Sender: TObject);
+begin
+  StartExe(TreeView.Path, 'meld', ['.'], True);
+  QueueForUpdate(TShellTreeNode(TreeView.Selected));
+end;
+
+procedure TFMain.MenuItemPullClick(Sender: TObject);
+var
+  OK: Boolean = False;
+  O: String = '';
+begin
+  if RunTool(TreeView.Path, GitExe, ['stash'], O) then
+    if RunTool(TreeView.Path, GitExe, ['pull', '--rebase'], O) then
+      if RunTool(TreeView.Path, GitExe, ['stash', 'pop'], O) then
+        OK := True;
+  if not OK then
+    MessageDlg('Error', O, mtError, [mbOK], 0);
+  QueueForUpdate(TShellTreeNode(TreeView.Selected));
+end;
+
+procedure TFMain.MenuItemPushClick(Sender: TObject);
+var
+  O: String = '';
+begin
+  if not RunTool(TreeView.Path, GitExe, ['push'], O) then
+    MessageDlg('Error', O, mtError, [mbOK], 0);
+  QueueForUpdate(TShellTreeNode(TreeView.Selected));
+end;
+
+procedure TFMain.NodeMenuPopup(Sender: TObject);
+var
+  Conflict: Boolean;
+  Git: Boolean;
+begin
+  Git := TreeView.Selected.Data <> nil;
+  Conflict := (Pos('[CONFLICT]', TreeView.Selected.Text) > 0);
+  MenuItemPull.Enabled := Git and not Conflict;
+  MenuItemPush.Enabled := Git and not Conflict;
+  MenuItemCommit.Enabled := Git and not Conflict;
+  MenuItemMeld.Enabled := Git;
 end;
 
 procedure TFMain.FormCreate(Sender: TObject);
@@ -250,12 +340,6 @@ begin
   if PI > 0 then begin
     Node.SelectedIndex := PI - 1;
   end;
-end;
-
-procedure TFMain.TreeViewMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-begin
-  //if Button = mbRight then
-  //  NodeMenu.PopUp;
 end;
 
 end.
