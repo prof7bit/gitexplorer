44d406971b6c6b2fb6cb64b43bd62f56d4e03a9f
Bernd Kreuss <b.kreuss@xecro.com>
2017-02-07 09:25:16 +0100

    TLockedQueue add license (LGPL with static linking exception)

--- LockedQueue.pas
+++ LockedQueue.pas
@@ -1,3 +1,33 @@
+{ Thread safe generic queue implementation with optional async notification
+  of main thread, WaitWor, non-blocking or blocking Get() with Timeout, etc.
+
+  Copyright (C) 2017 Bernd Kreuss <prof7bit@gmail.com>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version with the following modification:
+
+  As a special exception, the copyright holders of this library give you
+  permission to link this library with independent modules to produce an
+  executable, regardless of the license terms of these independent modules,and
+  to copy and distribute the resulting executable under terms of your choice,
+  provided that you also meet, for each linked independent module, the terms
+  and conditions of the license of that module. An independent module is a
+  module which is not derived from or based on this library. If you modify
+  this library, you may extend this exception to your version of the library,
+  but you are not obligated to do so. If you do not wish to do so, delete this
+  exception statement from your version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+}
 unit LockedQueue;
 
 {$mode objfpc}{$H+}
@@ -23,10 +53,11 @@ type
       function Get(out Element: T; Timeout: Cardinal): Boolean;
       function Get(out Element: T): Boolean;
       procedure Put(Element: T);
-      procedure PutArray(FirstElement: PT; Count: Integer);
+      procedure PutArray(FirstElement: PT; Count: SizeUint);
       procedure PutFront(Element: T);
       procedure Lock;
       procedure Unlock;
+      function Size: SizeUint;
       function IsEmpty: Boolean;
       function WaitFor(Timeout: Cardinal): TWaitResult;
     private
@@ -81,14 +112,14 @@ procedure TLockedQueue.Put(Element: T);
 begin
   Lock;
   FData.PushBack(Element);
-  if FData.Size() = 1 then begin
+  if Size() = 1 then begin
     FEvent.SetEvent;
     CallOnDataSync;
   end;
   Unlock;
 end;
 
-procedure TLockedQueue.PutArray(FirstElement: PT; Count: Integer);
+procedure TLockedQueue.PutArray(FirstElement: PT; Count: SizeUint);
 var
   I: Integer;
 begin
@@ -96,7 +127,7 @@ begin
   for I := 0 to Count - 1 do begin
     FData.PushBack(FirstElement[I]);
   end;
-  if FData.Size() = Count then begin
+  if Size() = Count then begin
     FEvent.SetEvent;
     CallOnDataSync;
   end;
@@ -107,7 +138,7 @@ procedure TLockedQueue.PutFront(Element: T);
 begin
   Lock;
   FData.PushFront(Element);
-  if FData.Size() = 1 then begin
+  if Size() = 1 then begin
     FEvent.SetEvent;
     CallOnDataSync;
   end;
@@ -126,6 +157,11 @@ begin
       FEvent.ResetEvent;
   end;
   Unlock;
+end;
+
+function TLockedQueue.Size: SizeUint;
+begin
+  Result := FData.Size;
 end;
 
 function TLockedQueue.IsEmpty: Boolean;
